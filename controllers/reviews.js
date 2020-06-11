211b2a5e447b2f042a0237686b6d599ba59c2b1c
guyett92 <aaronguyett@yahoo.com>
2020-06-10 20:17:45 -0700

    testing reviews

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -5,32 +5,30 @@ module.exports = {
     create
 };
 
-//FIXME: Point to a user correctly and make sure it is saving data correctly
 function create(req, res) {
-    User.find({}, function(err, users) {
-        Drug.findById(req.params.id, function(err, drug) {
-            //Point to the user who made the review
-            // drug.reviews.postedBy = req.user;
-            //Convert liked and side effects checkbox to booleans
-            req.body.liked = !!req.body.liked;
-            req.body.sideEffect = !!req.body.sideEffect;
-            //If it is liked, add it to user's array of liked drugs
-            // if (req.body.liked) {
-            //     user.liked.push(drug.name);
-            // }
-            //Remove blank fields
-            for (let key in req.body) {
-                if (req.body[key] === '') delete req.body[key];
-              }
-            drug.reviews.push(req.body);
-            if(err) {console.log(err)};
-            drug.save(function(err) {
-                console.log(err);
-                // console.log(user);
-                if(err) return res.redirect(`/drugs/${drug._id}`);
-                console.log(drug);
-                res.redirect(`/drugs`);
-            });
-        });
+    Drug.findById(req.params.id, function(err, drug) {
+        //Point to the user who made the review
+        drug.reviews.postedBy = req.user._id;
+        //Convert liked and side effects checkbox to booleans
+        req.body.liked = !!req.body.liked;
+        req.body.sideEffect = !!req.body.sideEffect;
+        //If it is liked, add it to user's array of liked drugs
+        if (req.body.liked) {
+            req.user.liked.push(drug.name);
+            req.user.save(function(err) {
+                //Remove blank fields
+                for (let key in req.body) {
+                    if (req.body[key] === '') delete req.body[key];
+                    }
+                drug.reviews.push(req.body);
+                if(err) {console.log(err)};
+                drug.save(function(err) {
+                    console.log(err);
+                    if(err) return res.redirect(`/drugs/${drug._id}`);
+                    console.log(drug);
+                    res.redirect(`/drugs`);
+                });
+            })
+        }
     });
 }
